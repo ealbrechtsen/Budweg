922f14569fdc160541bd310674e002ffdb126572
ealbrechtsen <emilmalikalbrechtsen@gmail.com>
2023-10-03 14:37:24 +0200

    Upload work of the day

---
+++ Ansat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budweg
+{
+    class Ansat
+    {
+        public string Navn { get; set; }
+        public string Telefonnummer { get; set; }
+        public string Email { get; set; }
+        public string Fødselsdag { get; set; }
+        public string Erfaring { get; set; }
+    }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Check ind som ansat.");
+            Console.WriteLine("2. Check ind som gæst.");
+            Console.WriteLine("3. Check ind som admin.");
+            Console.WriteLine("5. Vælg evakueringsliste");
+            Console.WriteLine("5. Tryk 1, 2 eller ...");
+
         }
     }
 }
